9168e217c00bdd5e684bc8b89e8eaed73495ef9a
leeSeongMin <zxc9436@naver.com>
2021-06-20 21:44:57 +0900

    2page complete

---
+++ src/main/java/org/example/myproject/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package org.example.myproject.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
